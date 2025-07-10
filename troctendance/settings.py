8b9c1dae752e3d891e927777836d808bfaf167ac
Mouha7 <ra.sow25@gmail.com>
2025-07-10 15:02:37 +0000

    fix: Ajouter 'troctendance.onrender.com' à ALLOWED_HOSTS dans settings.py

--- troctendance/settings.py
+++ troctendance/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'troctendance.onrender.com']
 
 
 # Application definition
